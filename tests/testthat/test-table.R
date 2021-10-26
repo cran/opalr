e40cd7a9bcfbab092cabcdc66e46e94ed866acc7
Yannick Marcon <yannick.marcon@obiba.org>
2021-10-26 08:10:02 +0000

    version 3.0.2

--- tests/testthat/test-table.R
+++ tests/testthat/test-table.R
@@ -50,7 +50,6 @@ test_that("Table import", {
   opal.logout(o)
 })
 
-
 test_that("Table save and get", {
   check_skip()
   #skip("Temporary skip")
@@ -65,7 +64,45 @@ test_that("Table save and get", {
   expect_equal(summary(ds2$cyl), summary(ds$cyl))
   expect_equal(summary(as.factor(ds2$vs)), summary(as.factor(ds$vs)))
   expect_true(all(colnames(ds) %in% colnames(ds2)))
+  
+  dico <- opal.table_dictionary_get(o, "RSRC", "mtcars")
+  expect_true(!all(dico$variables$repeatable))
+  
   opal.table_delete(o, "RSRC", "mtcars")
+  
+  # check no R session created
+  expect_null(o$rid)
+  
+  opal.logout(o)
+})
+
+test_that("Table with repeatables save and get", {
+  check_skip()
+  #skip("Temporary skip")
+  o <- opal.login("administrator", "password")
+  
+  ds <- make_test_dataset_with_repeatables()
+  opal.table_save(o, ds, "RSRC", "repeatables", force = TRUE)
+  expect_true(opal.table_exists(o, "RSRC", "repeatables"))
+  ds2 <- opal.table_get(o, "RSRC", "repeatables")
+  
+  ds1 <- ds[order(ds$id),]
+  ds1$id <- as.character(ds1$id)
+  expect_true(all.equal(ds1, ds2, check.attributes = FALSE))
+  expect_equal(nrow(ds2), nrow(ds))
+  expect_equal(ncol(ds2), ncol(ds))
+  expect_equal(summary(ds2$var1), summary(ds$var1))
+  expect_equal(summary(as.factor(ds2$sex)), summary(as.factor(ds$sex)))
+  expect_true(all(colnames(ds) %in% colnames(ds2)))
+  
+  ds1.nas <- ds1[is.na(ds1$sex),]
+  ds2.nas <- ds2[is.na(ds2$sex),]
+  expect_true(all.equal(ds1.nas, ds2.nas, check.attributes = FALSE))
+  
+  dico <- opal.table_dictionary_get(o, "RSRC", "repeatables")
+  expect_true(all(dico$variables$repeatable))
+  
+  opal.table_delete(o, "RSRC", "repeatables")
   
   # check no R session created
   expect_null(o$rid)
